03e6e14c0b1fdf6583d44740ab3ecc50ef7f07c1
Marios Papachristou <papachristoumarios@gmail.com>
2019-04-26 16:36:07 +0300

    Working LIMBO Implementation

--- sade/limbo.py
+++ sade/limbo.py
@@ -1,3 +1,9 @@
+"""
+    LIMBO Clustering Algorithm implementation
+    DOI: 10.1109/TSE.2005.25
+    Usage: limbo.py -h
+"""
+
 from scipy.sparse import *
 import argparse
 import copy
@@ -6,6 +12,9 @@ import numpy as np
 import sys
 import bisect
 import collections
+import sade.helpers
+np.warnings.filterwarnings('ignore')
+
 
 def kl_div(p, q):
     temp = - np.log (p / q)
@@ -13,267 +22,207 @@ def kl_div(p, q):
     return np.nansum(prod)
 
 
-
-class BTreeNode:
-
-    def __init__(self, B, leaf):
-        # Degree and leaf property
-        self.B = B
-        self.leaf = leaf
-
-        # Keys and children
-        self.keys = (2*B - 1)*[None]
-        self.children = (2*B) * [None]
-
-        # Number of keys
-        self.n = 0
-
-    def __repr__(self):
-        return 'Keys: {} Children: {}'.format(str(self.keys), str(self.children))
-
-    def search(self, key):
-        i = 0
-
-        while i < self.n and key > keys[i]:
-            i += 1
-
-        if keys[i] == key:
-            return self
-
-        if self.leaf:
-            return None
-
-        return self.children[i].search(key)
-
-    def insert_non_full(self, key):
-
-        i = self.n - 1
-
-        if self.leaf:
-            # Insert the key
-
-            while i >= 0 and self.keys[i] > key:
-                self.keys[i + 1] = self.keys[i]
-                i -= 1
-
-            self.keys[i+1] = key;
-
-            self.n += 1
-
-        else:
-
-            while i >= 0 and self.keys[i] > key:
-                i -= 1
-
-            if self.children[i + 1].n == 2 * self.B - 1:
-                self.split_child(i + 1, self.children[i + 1])
-
-                if self.keys[i + 1] < key:
-                    i += 1
-
-            self.children[i + 1].insert_non_full(key)
-
-    def split_child(self, i, y):
-
-        # Shallow copy
-        z = copy.copy(y)
-        z.n = self.B - 1
-
-        for j in range(self.B - 1):
-            z.keys[j] = y.keys[j + self.B]
-
-        if not y.leaf:
-            for j in range(self.B - 1):
-                z.children[j] = y.children[j + self.B]
-        y.n = self.B - 1
-
-        for j in range(self.n, i, -1):
-            self.children[j + 1] = self.children[j]
-
-        self.children[i + 1] = z
-
-        for j in range(self.n - 1, i - 1, -1):
-            self.keys[j+1] = self.keys[j]
-
-        self.keys[i] = y.keys[self.B-1];
-
-        self.n += 1
-
-    def collect_leaves(self, result):
-        if self.leaf:
-            print('foo')
-            result.append(self)
-            return result
-        else:
-            for i in range(self.n):
-                self.children[i].collect_leaves(result)
-
-class BTree:
-
+class DCFNode:
+    '''
+    DCF Node object
+    Args:
+        B (int): The maximum number of keys each node can hold.
+    '''
     def __init__(self, B):
         self.B = B
-        self.root = None
+        self.keys = []
+        self.values = []
+        self.leaf = True
+        self.merged = None
+        self.dI = None
 
-    def traverse(self):
-        if self.root != None:
-            self.root.traverse()
+    def add(self, key, value):
+        '''
+        Adds a key-value pair to the node.
+        '''
+        if not self.keys:
+            self.keys.append(key)
+            self.values.append([value])
+            return None
 
-    def search(self, key):
-        if self.root != None:
-            self.root.search(key)
+        for i, item in enumerate(self.keys):
+            if key == item:
+                self.values[i].append(value)
+                break
+
+            elif key < item:
+                self.keys = self.keys[:i] + [key] + self.keys[i:]
+                self.values = self.values[:i] + [[value]] + self.values[i:]
+                break
+
+            elif i + 1 == len(self.keys):
+                self.keys.append(key)
+                self.values.append([value])
+                break
+
+    def split(self):
+        '''
+        Splits the node into two and stores them as child nodes.
+        '''
+        left = DCFNode(self.B)
+        right = DCFNode(self.B)
+        mid = self.B // 2
+
+        left.keys = self.keys[:mid]
+        left.values = self.values[:mid]
+
+        right.keys = self.keys[mid:]
+        right.values = self.values[mid:]
+
+        self.keys = [right.keys[0]]
+        self.values = [left, right]
+        self.leaf = False
+
+    def is_full(self):
+        '''
+        Returns True if the node is full.
+        '''
+        return len(self.keys) == self.B
+
+    def __repr__(self):
+        if not self.leaf:
+            return 'Keys: {} Children: {}'.format(str(self.keys), str(self.values))
+        else:
+            return 'Keys: {}'.format(str(self.keys))
+
+    def merge(self):
+        ''' Merges a node's keys to a cluster '''
+        if len(self.keys) == 1:
+            self.merged = self.keys[0]
+            self.dI = 0
+        elif len(self.keys) >= 2:
+            temp_c, temp_dI = Cluster.merge_clusters(self.keys[0], self.keys[1])
+            for i in range(2, len(self.keys)):
+                temp_c, temp_dI = Cluster.merge_clusters(temp_c, self.keys[i])
+            self.merged = temp_c
+            self.dI = temp_dI
+        return self.merged
+
+
+
+class DCFTree:
+    '''
+    DCF Tree
+    Implementation of B+ Tree: https://gist.github.com/savarin/69acd246302567395f65ad6b97ee503d
+    Args:
+        B (int): The maximum number of keys each node can hold.
+    '''
+    def __init__(self, B=3, S=sys.maxsize, E=sys.maxsize):
+        self.root = DCFNode(B)
+        # XXX Implement space bound
+        self.max_number_of_nodes = S // (E * B)
+        self.number_of_nodes = 0
+
+    def _find(self, node, key):
+        '''
+        For a given node and key, returns the index where the key should be
+        inserted and the list of values at that index.
+        The index corresponds to the minimum information loss
+        '''
+        minimum_dI = sys.maxsize
+        argmin = -1
+
+        for i, item in enumerate(node.keys):
+            _, dI = Cluster.merge_clusters(key, item)
+            if dI <= minimum_dI:
+                minimum_dI = dI
+                argmin = i
+
+
+        return node.values[i], i
+
+    def _merge(self, parent, child, index):
+        '''
+        For a parent and child node, extract a pivot from the child to be
+        inserted into the keys of the parent. Insert the values from the child
+        into the values of the parent.
+        '''
+        parent.values.pop(index)
+        pivot = child.keys[0]
+
+        for i, item in enumerate(parent.keys):
+            if pivot < item:
+                parent.keys = parent.keys[:i] + [pivot] + parent.keys[i:]
+                parent.values = parent.values[:i] + child.values + parent.values[i:]
+                break
+
+            elif i + 1 == len(parent.keys):
+                parent.keys += [pivot]
+                parent.values += child.values
+                break
+
+    def insert(self, key, value):
+        '''
+        Inserts a key-value pair after traversing to a leaf node. If the leaf
+        node is full, split the leaf node into two.
+        '''
+        parent = None
+        child = self.root
+
+        while not child.leaf:
+            parent = child
+            child, index = self._find(child, key)
+
+        child.add(key, value)
+
+        if child.is_full():
+            child.split()
+
+            if parent and not parent.is_full():
+                self._merge(parent, child, index)
+
+        self.number_of_nodes += 1
+
+
+    def retrieve(self, key):
+        '''
+        Returns a value for a given key, and None if the key does not exist.
+        '''
+        child = self.root
+
+        while not child.leaf:
+            child, index = self._find(child, key)
+
+        for i, item in enumerate(child.keys):
+            if key == item:
+                return child.values[i]
+
+        return None
 
     def __repr__(self):
         return self.root.__repr__()
 
-    def insert(self, key):
-
-        # Root is created
-        if self.root == None:
-            self.root = BTreeNode(self.B, True)
-            self.root.keys[0] = key
-            self.root.n = 1
-
-        else:
-            # The B-Tree is full so we need to change the root by splitting
-            if self.root.n == 2 * self.B - 1:
-                s = BTreeNode(self.B, False)
-                s.children[0] = self.root
-
-                # Split and insert
-                s.split_child(0, self.root)
-
-                i = 0
-                if s.keys[0] < key:
-                    i += 1
-
-                s.children[i].insert_non_full(key)
-
-                # Change root
-                self.root = s
-
-            else:
-
-                self.root.insert_non_full(key)
-
-class DCFNode(BTreeNode):
-
-    def __init__(self, B, leaf):
-        super(DCFNode, self).__init__(B, leaf)
-        self._merged = None
-        self._dI = None
-
-    def merge(self):
-        if self.n == 1:
-            self._dI = 0
-            self.merged = self.keys[0]
-        elif self.n > 1:
-            temp_c, temp_dI = Cluster.merge_clusters(self.keys[0], self.keys[1])
-            for i in range(2, self.n):
-                temp_c, temp_dI = Cluster.merge_clusters(temp_c, self.keys[i])
-            self._merged = temp_c
-            self._dI = temp_dI
-        return self._merged
-
-    def insert_non_full(self, key):
-
-        i = self.n - 1
-
-        argmin = 0
-        minimum_dI = sys.maxsize
-
-        for j in range(self.n):
-            _, temp_dI = Cluster.merge_clusters(self.keys[j], key)
-            if temp_dI < minimum_dI:
-                minimum_dI = temp_dI
-                argmin = j
-
-        if self.leaf:
-            # Insert the key
-
-            while i >= argmin:
-                self.keys[i + 1] = self.keys[i]
-                i -= 1
-
-            self.keys[i+1] = key;
-
-            self.n += 1
-
-        else:
-
-            while i >= argmin: i -= 1
-
-            if self.children[i + 1].n == 2 * self.B - 1:
-                self.split_child(i + 1, self.children[i + 1])
-
-                if self.keys[i + 1] < key:
-                    i += 1
-
-            self.children[i + 1].insert_non_full(key)
-
-class DCFTree(BTree):
-
-    def __init__(self, B, S):
-        super(DCFTree, self).__init__(B)
-        self.S = S
-
-    def insert(self, key):
-
-        # Root is created
-        if self.root == None:
-            self.root = DCFNode(self.B, True)
-            self.root.keys[0] = key
-            self.root.n = 1
-
-        else:
-            # The B-Tree is full so we need to change the root by splitting
-            if self.root.n == 2 * self.B - 1:
-                s = DCFNode(self.B, False)
-                s.children[0] = self.root
-
-                # Split and insert
-                s.split_child(0, self.root)
-
-                i = 0
-                if s.keys[0] < key:
-                    i += 1
-
-                s.children[i].insert_non_full(key)
-
-                # Change root
-                self.root = s
-
-            else:
-
-                self.root.insert_non_full(key)
-
-
-    def border(self):
-        self.leaves = []
+    def cluster_leaves(self):
+        ''' Cluster the leaves of the tree for use with AIB '''
         q = collections.deque([self.root])
+        self.leaves = []
 
         while q:
             current = q.popleft()
             if current.leaf:
                 self.leaves.append(current)
             else:
-                for child in current.children:
-                    if child != None:
-                        q.append(child)
+                for child in current.values:
+                    q.append(child)
 
-        return self.leaves
+        clusters = []
+        for leaf in self.leaves:
+            leaf.merge()
+            if leaf.dI != None:
+                clusters.append(leaf.merged)
 
-
-    def cluster_leaves(self):
-        self.border()
-        self.clusters = [b.merge() for b in self.leaves]
-        return self.clusters
+        return clusters
 
 
 class Cluster:
 
     def __init__(self, features, feature_vector, num_tuples, total_tuples):
         self.features = features
-        # self.features.sort()
         self.feature_vector = feature_vector
         self.num_tuples = num_tuples
         self.total_tuples = total_tuples
@@ -329,7 +278,6 @@ def agglomerative_information_bottleneck_clustering(initial_clusters, n_clusters
 
     n = len(initial_clusters)
 
-
     while n > n_clusters:
         # Minimum indexes
         argmin_i = 0
@@ -372,7 +320,7 @@ def agglomerative_information_bottleneck_clustering(initial_clusters, n_clusters
     for i, cluster in enumerate(initial_clusters):
         result[i] = list(cluster.features)
 
-    return result    
+    return result
 
 
 def limbo(initial_clusters, n_clusters, B, S):
@@ -389,7 +337,7 @@ def limbo(initial_clusters, n_clusters, B, S):
     dcf_tree = DCFTree(B, S)
 
     for cluster in initial_clusters:
-        dcf_tree.insert(cluster)
+        dcf_tree.insert(cluster, cluster)
 
     # Phase 2: Apply AIB Algorithm to the tree leaves
     # Collect merged leaves
@@ -414,28 +362,6 @@ def preprocess_bow(bow_filename):
     return initial_clusters
 
 
-# if __name__  == '__main__':
-#     t1 = np.array([100, 1, 1, 1])
-#     t2 = np.array([0, 0, 2, 0])
-#     c1 = Cluster(('t1',), t1, 1, 2)
-#     c3 = Cluster(('t2',), t1, 1, 2)
-#     c2 = Cluster(('t3',), t2, 1, 2)
-#
-#     l = [c1, c2, c3]
-#
-#     res = agglomerative_information_bottleneck_clustering(l, 2)
-#
-#     print(res)
-#
-# t = DCFTree(3, 2)
-# N = 100
-# for i in range(N):
-#     tt = np.random.randint(100, size=4)
-#     c = Cluster(('t' + str(i),), tt, 1, N)
-#     t.insert(c)
-#
-# print(t.border)
-
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(usage='LIMBO Clustering Algorithm')
 
@@ -444,13 +370,18 @@ if __name__ == '__main__':
     argparser.add_argument('--aib', help='Naively run Agglomerative Information Bottleneck', action='store_true')
     argparser.add_argument('-B', help='DCF-tree branching factor', type=int, default=3)
     argparser.add_argument('-S', help='DCF-tree space factor', type=int, default=sys.maxsize)
+    argparser.add_argument('--export', help='Export type', default='bunch')
 
     args = argparser.parse_args()
 
     initial_clusters = preprocess_bow(args.i)
 
-
     if args.aib:
-        print(agglomerative_information_bottleneck_clustering(initial_clusters=initial_clusters, n_clusters=args.n))
+        result = agglomerative_information_bottleneck_clustering(initial_clusters=initial_clusters, n_clusters=args.n)
     else:
-        print(limbo(initial_clusters=initial_clusters, n_clusters=args.n, B=args.B, S=args.S))
+        result = limbo(initial_clusters=initial_clusters, n_clusters=args.n, B=args.B, S=args.S)
+
+    if args.export == 'bunch':
+        print(sade.helpers.generate_bunch(result))
+    elif args.export == 'json':
+        print(json.dumps(result))
